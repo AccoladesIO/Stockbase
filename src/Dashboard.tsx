a501e66d30a31d92d88ce6b7e355d4287fb6c7d3
unknown <ononobip@gmail.com>
2023-02-15 23:25:20 +0100

    ref: refractored this code

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from '../src/firebase'
+
 
 const Dashboard = () => {
+
+    useEffect(()=>{
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+              // User is signed in, see docs for a list of available properties
+              // https://firebase.google.com/docs/reference/js/firebase.User
+              const uid = user.uid;
+              // ...
+              console.log("uid", uid)
+              console.log(user)
+            } else {
+              // User is signed out
+              // ...
+              console.log("user is logged out")
+            }
+          });
+         
+    }, [])
+
   return (
     <div>Dashboard</div>
   )
